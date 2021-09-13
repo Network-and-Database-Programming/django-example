655fcb2c968499f69f47bcbb8b145f447a820145
ZoneTwelve <zonetwelve159@gmail.com>
2021-09-13 10:16:58 +0800

    [update] fixing security issues

--- application/settings.py
+++ application/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
 from pathlib import Path
 
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t&8319o5&y)v)c(=!!znq(px56d)7$e=xe!0$gsiyeo6@2^8*0'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") # recommand at lest 64 character
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG_MODE") == "true" if True else False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOW_HOSTS").split(",")
 
 
 # Application definition
